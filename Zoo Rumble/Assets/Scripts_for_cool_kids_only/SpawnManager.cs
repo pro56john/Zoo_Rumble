5e88721c1c9d0283f93d6b51219d158ae1c210df
Riccatisthebest <riccardo.volonte@sis-basel.ch>
2023-09-28 15:01:13 +0200

    Spawnmanager update not finished

--- Zoo Rumble/Assets/Scripts_for_cool_kids_only/SpawnManager.cs
+++ Zoo Rumble/Assets/Scripts_for_cool_kids_only/SpawnManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+    void SpawnRandomAnimal()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, Prefabs.Length);
+            Instantiate(Prefabs[animalIndex], spawnPos, Prefabs[animalIndex].transform.rotation);
+        }
     }
 }
