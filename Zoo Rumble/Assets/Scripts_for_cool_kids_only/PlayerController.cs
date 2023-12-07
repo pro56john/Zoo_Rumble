8dd2a3df435fff3de5a0d0e6b304b25b79467f60
Riccatisthebest <riccardo.volonte@sis-basel.ch>
2023-12-07 14:08:51 +0100

    left and right movement

---
+++ Zoo Rumble/Assets/Scripts_for_cool_kids_only/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float move, moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 15f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+    }
+    private void LateUpdate()
+    {
+        transform.Translate(0f, 0f, move);
+        transform.Translate(move, 0f, 0f);
+    }
+}
