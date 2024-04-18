1dfe537bbffb9a9c0208a8d1bf728c38bf78183c
Riccatisthebest <142297713+Riccatisthebest@users.noreply.github.com>
2024-04-18 14:19:01 +0200

    main menu update

--- Zoo Rumble/Assets/Scripts_for_cool_kids_only/DestroyOutOfBounds.cs
+++ Zoo Rumble/Assets/Scripts_for_cool_kids_only/DestroyOutOfBounds.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
-    private float lowerBound;
-    private Vector3 positionBeginning;
-    public float playerZ;
+    private float lowerBound; // Variablen fuer den Code
+    private Vector3 positionBeginning; // Variablen fuer den Code (speziell fuer eine Nummer)
+    public float playerZ; // Variablen fuer den Code
     private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Player");
-        positionBeginning = player.transform.position;
+        player = GameObject.Find("Player"); // Es sucht und findet den Spieler 
+        positionBeginning = player.transform.position; // Es sucht nach der Spieler position
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerZ = player.transform.position.z;
-        lowerBound = playerZ - 20f;
+        playerZ = player.transform.position.z; 
+        lowerBound = playerZ - 20f; // Dies erstellt eine Boundrie dies sich mit dem Spieler bewegt
         if (transform.position.z < lowerBound)
         {
-            Destroy(gameObject);
+            Destroy(gameObject); // Dies zersoert die Objekten die ausserhalb von der Boundrie gehen
         }
     }
 }
--- Zoo Rumble/Assets/Scripts_for_cool_kids_only/ElephantSpeed.cs
+++ Zoo Rumble/Assets/Scripts_for_cool_kids_only/ElephantSpeed.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class ElephantSpeed : MonoBehaviour
 {
-    private float speed;
+    private float speed; // Variablen für den Code
     // Start is called before the first frame update
     void Start()
     {
-        speed = Random.Range(40, 80);
+        speed = Random.Range(40, 80); // findet eine nummer zwischen 40-80 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        transform.Translate(Vector3.right * speed * Time.deltaTime); // Benutzt die Variable somit die Elefanten sich fortbewegen mit der schneligkeit von 40-80
     }
 }
