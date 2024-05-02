75938c9ed7589f5d2aa70e1fce1993a0fdf3cebf
Riccatisthebest <142297713+Riccatisthebest@users.noreply.github.com>
2024-05-02 15:09:57 +0200

    finished only missing the coins

--- Zoo Rumble/Assets/Scripts_for_cool_kids_only/GameOverScript.cs
+++ Zoo Rumble/Assets/Scripts_for_cool_kids_only/GameOverScript.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class GameOverScript : MonoBehaviour
 {
     public void Setup(int score)
     {
         gameObject.SetActive(true);
+    }
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
